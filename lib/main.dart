c1f182bc4d3d2200e1a5c345b63dbb0c47a5c6bf
hackinsubho <subho3010@gmail.com>
2018-04-01 14:37:34 +0530

    added setState to the app

--- lib/main.dart
+++ lib/main.dart
@@ -72,9 +72,20 @@ class RandomWordsState extends State <RandomWords>{
        style: _biggerFont,
      ),
      trailing: new Icon(
-       alreadySaved ? Icons.favorite: Icons.favorite_border,
-       color: alreadySaved ? Colors.red: null,
+       alreadySaved ? Icons.favorite : Icons.favorite_border,
+       color: alreadySaved ? Colors.red : null,
      ),
+     onTap: () {
+       setState(
+           (){
+             if (alreadySaved) {
+               _saved.remove(pair);
+             }else {
+               _saved.add(pair);
+             }
+           }
+           );
+     }
    );
+     }
  }
-}
